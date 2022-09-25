eb66b17d21d272212480a282aeb972d10f207b80
Andrey1914 <anlikajw@gmail.com>
2022-09-25 21:11:04 +0200

    added redux and routes folders

---
+++ src/redux/selectors.jsx
@@ -0,0 +1,10 @@
+export const contactSelector = state => state.contacts;
+export const filterSelector = state => state.filter;
+
+export const filterContacts = (state, contacts) => {
+  const filter = filterSelector(state);
+  const normalizeFilter = filter?.toLowerCase();
+  return contacts?.filter(contact =>
+    contact?.toLowerCase().includes(normalizeFilter)
+  );
+};
---
+++ src/redux/store.jsx
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { contactsApi } from './API';
+import { filter } from './reducer';
+
+export const store = configureStore({
+  reducer: {
+    [contactsApi.reducerPath]: contactsApi.reducer,
+    filter,
+  },
+  middleware: getDefaultMiddlewares =>
+    getDefaultMiddlewares().concat(contactsApi.middleware),
+
+  devTools: process.env.NODE_ENV !== 'production',
+});
+setupListeners(store.dispatch);
